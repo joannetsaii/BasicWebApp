826a8eac59eefb6dbaa2bd441a4075837e8d1858
joannetsaii <chihant@andrew.cmu.edu>
2022-09-20 19:25:04 -0400

    Added text in IndexPage.java

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,6 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
+                "<h3>I added something successfully :)</h3>" +
                 "<h1>Welcome!</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
